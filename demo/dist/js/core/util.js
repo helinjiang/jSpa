33eb1671a983db351b26109b7b329684a2408ce8
helinjiang <me@helinjiang.com>
2015-12-02 17:26:13 +0800

    简化用例

--- demo/dist/js/core/util.js
+++ demo/dist/js/core/util.js
@@ -4,56 +4,10 @@
  * @author linjianghe
  * @date   2015-07-16
  */
-window.UTIL = (function(global, undefined) {
-    var type_text = ['unknown', 'ethernet', 'wifi', '2g', '3g', '4g', 'none'];
-
-
-    /**
-     * 获得当前的网络状况
-     * @return {string}   ethernet、wifi、3g、2g、none、unknown
-     * @author linjianghe
-     * @date   2015-09-18
-     */
-    function getNetwork() {
-        var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || {
-            type: 'unknown'
-        };
-        if (typeof(connection.type) === "number") {
-            connection.type_text = type_text[connection.type];
-        } else {
-            connection.type_text = connection.type;
-        }
-        if (typeof(connection.bandwidth) === "number") {
-            if (connection.bandwidth > 10) {
-                connection.type = 'wifi';
-            } else if (connection.bandwidth > 2) {
-                connection.type = '3g';
-            } else if (connection.bandwidth > 0) {
-                connection.type = '2g';
-            } else if (connection.bandwidth === 0) {
-                connection.type = 'none';
-            } else {
-                connection.type = 'unknown';
-            }
-        }
-        return connection.type_text;
-    }
-
-    /**
-     * 获得屏幕分辨率，例如1080x768
-     * @return {string}   屏幕分辨率
-     * @author linjianghe
-     * @date   2015-09-18
-     */
-    function getResolution() {
-        return [screen.width, screen.height].join("x");
-    }
+window.UTIL = (function() {
 
     /**
      * 重新加载页面，由于是单页面应用，有一些数据缓存，一般不使用这种方法来重加载，而是自定义函数重加载数据。
-     * 只有在登录成功之后等场景下，要重载的东西很多时，再使用该方法。
-     * @author linjianghe
-     * @date   2015-07-16
      */
     function reload() {
         location.reload();
@@ -61,71 +15,70 @@ window.UTIL = (function(global, undefined) {
 
     /**
      * 页面后退
-     *
-     * @author linjianghe
-     * @date   2015-08-31
      */
     function back() {
         history.back();
     }
 
+    /**
+     * 为某个dom设置html代码
+     * @param  {Object}   container     目标DOM
+     * @param  {String}   html          html代码
+     * @param  {Boolean}   append        是否是追加的方式，true:追加，false:覆盖
+     * @param  {Boolean}   triggerActive 是否触发active事件，true:触发，false:不处罚
+     */
+    function setHtml(container, html, append, triggerActive) {
+        var elem;
 
-    function getTimeStamp(timeStamp) {
-        var tmp = timeStamp + "";
-        if (tmp.length < 11) {
-            timeStamp *= 1000;
+        if (container.innerHTML) {
+            // 如果该对象有innerHTML属性，说明是原生DOM对象
+            elem = container;
+        } else {
+            // 如果该对象没有innerHTML属性，说明可能是jQuery或者Zepto对象，当然，这也不一定，看实际情况            
+            elem = container[0];
         }
 
-        return timeStamp;
+        jSpa.setHtml(elem, html, append, triggerActive);
     }
 
     /**
-     * 获得DOM上的数据，寻找data-[key]的值
-     * @param  {Object} elem    DOM元素
-     * @param  {String} key     key值
-     * @param  {Bollean} upgoing 是否向上寻找，true：向上寻找，false：不用再向上寻找；
-     * @return {String}         结果
-     * @author linjianghe
-     * @date   2015-09-21
+     * 初始化jSpa框架
      */
-    function getDomData(elem, key, upgoing) {
-        if (!elem) {
-            return;
-        }
+    function initjSpa() {
+        jSpa.render({
+            //默认的页面ID，如果没有#id这样的参数的话
+            pageDefaultId: 'index',
 
-        var val;
+            //出错的页面ID，如果有#id=xxx，但xxx在页面中又没定义的话
+            page404Id: 'p404',
 
-        var data = function(curelem, curkey) {
-            var attr = curelem.attributes["data-" + curkey];
-            if (attr) {
-                return attr.value;
+            beforeLoad: function(target) {
+                console.log('beforeLoad: ', target);
+            },
+
+            afterLoad: function(target) {
+                console.log('afterLoad: ', target);
             }
-        };
+        });
+    }
 
-        if (upgoing) {
-            while (!elem.is("body")) {
-                val = elem.attr("data-" + key);
-                if (val) {
-                    break;
-                } else {
-                    elem = elem.parent();
-                }
-            }
-        } else {
-            val = elem.attr("data-" + key);
-        }
-        return val;
+    /**
+     * 跳转到哪个页面
+     * @param  {String}   pageName 页面名称
+     */
+    function go(pageName) {
+        jSpa.goToPage(pageName);
     }
 
     var exports = {
-        "getNetwork": getNetwork,
-        "getResolution": getResolution,
         "reload": reload,
         "back": back,
-        "getTimeStamp": getTimeStamp,
-        "getDomData": getDomData
+        "setHtml": setHtml,
+        "initjSpa": initjSpa,
+        "go": go,
+        "Page": jSpa.Page
     };
 
     return exports;
 
-})(window);
+})();
